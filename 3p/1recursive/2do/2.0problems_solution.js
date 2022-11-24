eaf766635c31c788a3760e039ce7d0ed6e4eb12a
BRANDON HILARIO ROMERO ALVAREZ <113148714+brandonhilarioromeroalvarez@users.noreply.github.com>
2022-11-24 06:15:13 +0000

    cambios

--- 3p/1recursive/2do/2.0problems_solution.js
+++ 3p/1recursive/2do/2.0problems_solution.js
@@ -1,10 +1,17 @@
 //fibonacci
 function fibonacci(n) {
-    if (n < 2) {//base case
-      return n
-    }
-        return fibonacci(n - 1)+fibonacci(n-2)//recursive call
+  if (n === 1) 
+  {
+    return [0,1];
+  } 
+  else 
+  {
+    var s = fibonacci(n - 1)
+    s.push(s[s.length - 1] + s[s.length - 2])
+    return s
+  }
 }
+
 //Potencia
 function power(a,b){
   if (b == 0)
@@ -19,11 +26,6 @@ function factorial(a){
   else
     return a * factorial(a-1)
 }
-console.log(fibonacci(0))
-console.log(fibonacci(2))
-console.log(fibonacci(3))
-console.log(fibonacci(4))
-console.log(fibonacci(5))
-
-console.log("Potencia: \n" + power(2,3) + "\n")
-console.log("Factorial: \n" + factorial(3))
+console.log(fibonacci(10))
+console.log(power(2,3))
+console.log(factorial(6))
--- 3p/3graphs/linked2do/2do/2do.js
+++ 3p/3graphs/linked2do/2do/2do.js
@@ -2,3 +2,25 @@
 // design node of a 
 // weighted graph incidence list 
 // 
+let incidencelist = {
+  "1":{"Nodo1":"a",
+       "Nodo2":"b",
+       "Peso":"7"},
+  "2":{"Nodo1":"b",
+       "Nodo2":"d",
+       "Peso":"4"},
+  "3":{"Nodo1":"d",
+       "Nodo2":"c",
+       "Peso":"2"},
+  "4":{"Nodo1":"c",
+       "Nodo2":"e",
+       "Peso":"1"},
+  "5":{"Nodo1":"e",
+       "Nodo2":"d",
+       "Peso":"9"},
+  "6":{"Nodo1":"e",
+       "Nodo2":"f",
+       "Peso":"6"},
+}
+
+console.log(incidencelist)
